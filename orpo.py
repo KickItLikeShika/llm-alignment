462bf3ddb1782852c5d9daa40f8481da8ff0aa68
KickItLikeShika <ahmedkhaled19701@gmail.com>
2025-02-24 11:50:43 +0000

    updated orpo code to be called in main

--- orpo.py
+++ orpo.py
@@ -1,123 +1,120 @@
 import gc
 import os
-
 import torch
-import wandb
 from datasets import load_dataset
 from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
-    TrainingArguments,
-    pipeline,
-)
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from trl import ORPOConfig, ORPOTrainer, setup_chat_format
 
-attn_implementation = "flash_attention_2"
-torch_dtype = torch.bfloat16
+def train_orpo(train_dataset=None):
+    attn_implementation = "flash_attention_2"
+    torch_dtype = torch.bfloat16 
+        
+    # Model
+    base_model = "meta-llama/Llama-3.2-1B"
+    new_model = "OrpoLlama-3.2-1B"
+
+    # QLoRA config
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch_dtype,
+        bnb_4bit_use_double_quant=True,
+    )
+
+    # LoRA config
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=['up_proj', 'down_proj', 'gate_proj', 'k_proj', 'q_proj', 'v_proj', 'o_proj']
+    )
+
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    tokenizer.pad_token = tokenizer.eos_token
     
-# Model
-# base_model = "unsloth/Llama-3.2-1B-Instruct"
-base_model = "meta-llama/Llama-3.2-1B-Instruct"
-new_model = "OrpoLlama-3.2-1B"
+    # Load model
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        quantization_config=bnb_config,
+        device_map="auto",
+        attn_implementation=attn_implementation
+    )
+    model = prepare_model_for_kbit_training(model)
 
-# QLoRA config
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch_dtype,
-    bnb_4bit_use_double_quant=True,
-)
+    if train_dataset is None:
+        dataset_name = "mlabonne/orpo-dpo-mix-40k"
+        dataset = load_dataset(dataset_name, split="all")
+        dataset = dataset.shuffle(seed=42).select(range(20000))
+    else:
+        dataset = train_dataset
 
-# LoRA config
-peft_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    lora_dropout=0.05,
-    bias="none",
-    task_type="CAUSAL_LM",
-    target_modules=['up_proj', 'down_proj', 'gate_proj', 'k_proj', 'q_proj', 'v_proj', 'o_proj']
-)
+    def format_chat_template(row):
+        row["chosen"] = tokenizer.apply_chat_template(row["chosen"], tokenize=False)
+        row["rejected"] = tokenizer.apply_chat_template(row["rejected"], tokenize=False)
+        return row
 
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(base_model)
-tokenizer.pad_token = tokenizer.eos_token
-# Load model
-model = AutoModelForCausalLM.from_pretrained(
-    base_model,
-    quantization_config=bnb_config,
-    device_map="auto",
-    attn_implementation=attn_implementation
-)
-# model, tokenizer = setup_chat_format(model, tokenizer)
-model = prepare_model_for_kbit_training(model)
+    dataset = dataset.map(
+        format_chat_template,
+        num_proc=os.cpu_count(),
+    )
 
-dataset_name = "mlabonne/orpo-dpo-mix-40k"
-dataset = load_dataset(dataset_name, split="all")
-dataset = dataset.shuffle(seed=42).select(range(20000))
+    dataset = dataset.train_test_split(test_size=0.01)
 
-def format_chat_template(row):
-    row["chosen"] = tokenizer.apply_chat_template(row["chosen"], tokenize=False)
-    row["rejected"] = tokenizer.apply_chat_template(row["rejected"], tokenize=False)
-    return row
+    orpo_args = ORPOConfig(
+        learning_rate=8e-6,
+        beta=0.1,
+        lr_scheduler_type="linear",
+        max_length=1024,
+        max_prompt_length=512,
+        per_device_train_batch_size=2,
+        per_device_eval_batch_size=2,
+        gradient_accumulation_steps=4,
+        optim="paged_adamw_8bit",
+        num_train_epochs=1,
+        evaluation_strategy="steps",
+        eval_steps=0.2,
+        logging_steps=20,
+        warmup_steps=10,
+        report_to="wandb",
+        output_dir="./results/",
+    )
 
-dataset = dataset.map(
-    format_chat_template,
-    num_proc= os.cpu_count(),
-)
+    trainer = ORPOTrainer(
+        model=model,
+        args=orpo_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        peft_config=peft_config,
+        tokenizer=tokenizer,
+    )
+    trainer.train()
+    trainer.save_model(new_model)
 
-dataset = dataset.train_test_split(test_size=0.01)
+    # Flush memory
+    del trainer, model
+    gc.collect()
+    torch.cuda.empty_cache()
 
-orpo_args = ORPOConfig(
-    learning_rate=8e-6,
-    beta=0.1,
-    lr_scheduler_type="linear",
-    max_length=1024,
-    max_prompt_length=512,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    gradient_accumulation_steps=4,
-    optim="paged_adamw_8bit",
-    num_train_epochs=1,
-    evaluation_strategy="steps",
-    eval_steps=0.2,
-    logging_steps=20,
-    warmup_steps=10,
-    report_to="wandb",
-    output_dir="./results/",
-)
+    # Reload tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    model, tokenizer = setup_chat_format(model, tokenizer)
 
-trainer = ORPOTrainer(
-    model=model,
-    args=orpo_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"],
-    peft_config=peft_config,
-    tokenizer=tokenizer,
-)
-trainer.train()
-trainer.save_model(new_model)
+    # Merge adapter with base model
+    model = PeftModel.from_pretrained(model, new_model)
+    model = model.merge_and_unload()
 
-# Flush memory
-del trainer, model
-gc.collect()
-torch.cuda.empty_cache()
+    model.push_to_hub(new_model, use_temp_dir=False)
+    tokenizer.push_to_hub(new_model, use_temp_dir=False)
 
-# Reload tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained(base_model)
-model = AutoModelForCausalLM.from_pretrained(
-    base_model,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.bfloat16,
-    device_map="auto",
-)
-model, tokenizer = setup_chat_format(model, tokenizer)
-
-# Merge adapter with base model
-model = PeftModel.from_pretrained(model, new_model)
-model = model.merge_and_unload()
-
-model.push_to_hub(new_model, use_temp_dir=False)
-tokenizer.push_to_hub(new_model, use_temp_dir=False)
+    return model, tokenizer
